133dfbae88e98a401e12ab7f92736c4ddf6aa248
zw9love <18514075699@163.com>
2018-02-08 16:35:13 +0800

    modify controller

--- src/main/java/com/roncoo/education/controller/IndexController.java
+++ src/main/java/com/roncoo/education/controller/IndexController.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import org.springframework.web.bind.annotation.*;
 
 import com.roncoo.education.bean.User;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,41 +23,37 @@ public class IndexController {
     //	@RequestMapping
 //	@RequestMapping(value = "/index")//	@GetMapping("/index")
     @GetMapping("/*")
-    public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String contextPath = request.getSession().getServletContext().getRealPath("/");
-        System.out.println("====================================");
-        System.out.println("contextPath = " + contextPath);
-        System.out.println("====================================");
+    public String index(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("utf-8");
         response.setContentType("application/json;charset=utf-8");
-        String path = request.getRequestURI();
-        if(path.equals("/"))
-            response.getWriter().write("首页");
-        else
-            response.sendRedirect("/");
-//		return "index方法。Hello SpringBoot!!!";
+        return "error";
+//        String path = request.getRequestURI();
+//        if (path.equals("/"))
+//            response.getWriter().write("首页");
+//        else
+//            response.sendRedirect("/");
 //        return "index";
     }
 
     // @RequestParam 简单类型的绑定，可以出来get和post
     // @RequestMapping(value = "/login",method=RequestMethod.GET) 还可规定请求方式
-    @RequestMapping(value = "/get")
-    public HashMap<String, Object> get(@RequestParam String name) {
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("title", "hello world");
-        map.put("name", name);
-//		System.out.println(map.toString());
-        return map;
-    }
+//    @RequestMapping(value = "/get")
+//    public HashMap<String, Object> get(@RequestParam String name) {
+//        HashMap<String, Object> map = new HashMap<String, Object>();
+//        map.put("title", "hello world");
+//        map.put("name", name);
+////		System.out.println(map.toString());
+//        return map;
+//    }
 
     // @PathVariable 获得请求url中的动态参数
-    @RequestMapping(value = "/get/{id}/{name}")
-    public User getUser(@PathVariable int id, @PathVariable String name) {
-        User user = new User();
-        user.setId(id);
-        user.setName(name);
-        user.setDate(new Date());
-        return user;
-    }
+//    @RequestMapping(value = "/get/{id}/{name}")
+//    public User getUser(@PathVariable int id, @PathVariable String name) {
+//        User user = new User();
+//        user.setId(id);
+//        user.setName(name);
+//        user.setDate(new Date());
+//        return user;
+//    }
 
 }
---
+++ src/main/java/com/roncoo/education/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.roncoo.education.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by admin on 2018/2/8.
+ */
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+}
