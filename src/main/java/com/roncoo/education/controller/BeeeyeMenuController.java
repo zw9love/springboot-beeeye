fa0ec1315b91b78c0d46e4f8b5d3dde7370e8937
zw9love <415789992@qq.com>
2018-06-18 10:00:41 +0800

    add menu mapper

--- src/main/java/com/roncoo/education/controller/BeeeyeMenuController.java
+++ src/main/java/com/roncoo/education/controller/BeeeyeMenuController.java
@@ -3,6 +3,7 @@ package com.roncoo.education.controller;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.roncoo.education.bean.Menu;
+import com.roncoo.education.mapper.MenuRowMapper;
 import com.roncoo.education.util.MyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -26,21 +27,21 @@ public class BeeeyeMenuController {
     @RequestMapping("get")
     public JSONObject get() throws JSONException {
         String select = " SELECT * FROM common_menu ";
-        List<Menu> list = jdbcTemplate.query(select, new RowMapper<Menu>() {
-            @Override
-            public Menu mapRow(ResultSet resultSet, int i) throws SQLException {
-//                System.out.println("index = " + i);
-                Menu menu = new Menu();
-                menu.setIds(resultSet.getString("ids"));
-                menu.setNames(resultSet.getString("names"));
-                menu.setLevel(resultSet.getInt("level"));
-                menu.setParent_ids(resultSet.getString("parent_ids"));
-                menu.setDescription(resultSet.getString("description"));
-                menu.setUrl(resultSet.getString("url"));
-                menu.setIcon(resultSet.getString("icon"));
-                return menu;
-            }
-        });
+        List<Menu> list = jdbcTemplate.query(select, new MenuRowMapper());
+//        List<Menu> list = jdbcTemplate.query(select, new RowMapper<Menu>() {
+//            @Override
+//            public Menu mapRow(ResultSet resultSet, int i) throws SQLException {
+//                Menu menu = new Menu();
+//                menu.setIds(resultSet.getString("ids"));
+//                menu.setNames(resultSet.getString("names"));
+//                menu.setLevel(resultSet.getInt("level"));
+//                menu.setParent_ids(resultSet.getString("parent_ids"));
+//                menu.setDescription(resultSet.getString("description"));
+//                menu.setUrl(resultSet.getString("url"));
+//                menu.setIcon(resultSet.getString("icon"));
+//                return menu;
+//            }
+//        });
         JSONObject jsonObj = MyUtil.getJson("成功", 200, list);
         return jsonObj;
     }
