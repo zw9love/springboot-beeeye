eccd9f799a698645576807f54730270ddb494693
zengwei <18514075699@163.com>
2018-06-22 16:44:12 +0800

    test code

--- src/main/java/com/roncoo/education/util/Test.java
+++ src/main/java/com/roncoo/education/util/Test.java
@@ -5,6 +5,8 @@ import java.util.Date;
 public class Test {
 
     public static void main(String[] args) {
+        // 拿到都是毫秒
+        System.out.println(System.currentTimeMillis());
         System.out.println(new Date().getTime());
     }
 }
