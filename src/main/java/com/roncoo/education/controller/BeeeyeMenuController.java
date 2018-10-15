d272db5e04f438d2aa922e3b083df100a1c6ee38
zengwei <18514075699@163.com>
2018-10-15 19:06:54 +0800

    menu controller use mybatis

--- src/main/java/com/roncoo/education/controller/BeeeyeMenuController.java
+++ src/main/java/com/roncoo/education/controller/BeeeyeMenuController.java
@@ -5,6 +5,10 @@ import com.alibaba.fastjson.JSONObject;
 import com.roncoo.education.bean.Menu;
 import com.roncoo.education.mapper.MenuRowMapper;
 import com.roncoo.education.util.MyUtil;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -27,43 +33,76 @@ import java.util.List;
 public class BeeeyeMenuController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    @RequestMapping("get")
-    public JSONObject get(HttpServletRequest request, HttpServletResponse response) throws JSONException {
-        // select * from common_menu where  names in (select record_hash from common_module_role where role_ids = '0' )
-        // select  lmr.ids as idss,common_menu.ids as ids, names, level, parent_ids, lmr.read r, lmr.write  w, url, icon   from common_menu " +
-        //                "left join common_module_role as lmr on lmr.module_ids=common_menu.ids " +
-        //                "where level>0 and lmr.role_ids=?"
-        HttpSession session = request.getSession();
-        String token = request.getHeader("token");
-        String role_ids;
-        if(token.equals("debug")){
-            role_ids = "0";
-        }else{
-            JSONObject role = (JSONObject) session.getAttribute(token);
-            role_ids = (String) role.get("roleIds");
+
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+    private static String resource = "mybatis.cfg.xml";
+
+    static {
+        try {
+            reader = Resources.getResourceAsReader(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(role_ids);
-//        String select = " SELECT * FROM common_menu where names in ( select record_hash from common_module_role where role_ids = ? )";
-        String select = "select lmr.ids as idss, common_menu.ids as ids, names, level, parent_ids, lmr.read r, lmr.write  w, url, icon from common_menu " +
-                "left join common_module_role as lmr on lmr.module_ids = common_menu.ids " +
-                "where level > 0 and lmr.role_ids = ?";
-        Object[] params = new Object[]{role_ids};
-        List<Menu> list = jdbcTemplate.query(select, params, new MenuRowMapper());
-//        List<Menu> list = jdbcTemplate.query(select, new RowMapper<Menu>() {
-//            @Override
-//            public Menu mapRow(ResultSet resultSet, int i) throws SQLException {
-//                Menu menu = new Menu();
-//                menu.setIds(resultSet.getString("ids"));
-//                menu.setNames(resultSet.getString("names"));
-//                menu.setLevel(resultSet.getInt("level"));
-//                menu.setParent_ids(resultSet.getString("parent_ids"));
-//                menu.setDescription(resultSet.getString("description"));
-//                menu.setUrl(resultSet.getString("url"));
-//                menu.setIcon(resultSet.getString("icon"));
-//                return menu;
-//            }
-//        });
-        JSONObject jsonObj = MyUtil.getJson("成功", 200, list);
-        return jsonObj;
+    }
+
+//    @RequestMapping("get")
+//    public JSONObject get(HttpServletRequest request, HttpServletResponse response) throws JSONException {
+//        // select * from common_menu where  names in (select record_hash from common_module_role where role_ids = '0' )
+//        // select  lmr.ids as idss,common_menu.ids as ids, names, level, parent_ids, lmr.read r, lmr.write  w, url, icon   from common_menu " +
+//        //                "left join common_module_role as lmr on lmr.module_ids=common_menu.ids " +
+//        //                "where level>0 and lmr.role_ids=?"
+//        HttpSession session = request.getSession();
+//        String token = request.getHeader("token");
+//        String role_ids;
+//        if(token.equals("debug")){
+//            role_ids = "0";
+//        }else{
+//            JSONObject role = (JSONObject) session.getAttribute(token);
+//            role_ids = (String) role.get("roleIds");
+//        }
+//        System.out.println(role_ids);
+////        String select = " SELECT * FROM common_menu where names in ( select record_hash from common_module_role where role_ids = ? )";
+//        String select = "select lmr.ids as idss, common_menu.ids as ids, names, level, parent_ids, lmr.read r, lmr.write  w, url, icon from common_menu " +
+//                "left join common_module_role as lmr on lmr.module_ids = common_menu.ids " +
+//                "where level > 0 and lmr.role_ids = ?";
+//        Object[] params = new Object[]{role_ids};
+//        List<Menu> list = jdbcTemplate.query(select, params, new MenuRowMapper());
+////        List<Menu> list = jdbcTemplate.query(select, new RowMapper<Menu>() {
+////            @Override
+////            public Menu mapRow(ResultSet resultSet, int i) throws SQLException {
+////                Menu menu = new Menu();
+////                menu.setIds(resultSet.getString("ids"));
+////                menu.setNames(resultSet.getString("names"));
+////                menu.setLevel(resultSet.getInt("level"));
+////                menu.setParent_ids(resultSet.getString("parent_ids"));
+////                menu.setDescription(resultSet.getString("description"));
+////                menu.setUrl(resultSet.getString("url"));
+////                menu.setIcon(resultSet.getString("icon"));
+////                return menu;
+////            }
+////        });
+//        JSONObject jsonObj = MyUtil.getJson("成功", 200, list);
+//        return jsonObj;
+//    }
+
+
+    // mybatis
+    @RequestMapping("get")
+    public JSONObject getmbt(HttpServletRequest request, HttpServletResponse response) throws JSONException {
+        SqlSession session = null;
+        try {
+            session = sqlSessionFactory.openSession();
+            List<Menu> list = session.selectList("selectMenu");
+            session.commit();
+            return MyUtil.getJson("成功", 200, list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+            return MyUtil.getJson("失败", 606, null);
+        } finally {
+            session.close();
+        }
     }
 }
--- src/main/java/com/roncoo/education/util/Test.java
+++ src/main/java/com/roncoo/education/util/Test.java
@@ -6,7 +6,8 @@ public class Test {
 
     public static void main(String[] args) {
         // 拿到都是毫秒
-        System.out.println(System.currentTimeMillis());
-        System.out.println(new Date().getTime());
+        // System.out.println(System.currentTimeMillis());
+        // System.out.println(new Date().getTime());
+        System.out.println(MyUtil.getRandomString()); // 随机24位字符串
     }
 }
